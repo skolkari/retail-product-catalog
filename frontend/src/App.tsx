9e47e89b41b71d7e54c784bc5d15d7065372cd11
skolkari <salmakolkari@gmail.com>
2024-09-24 06:57:48 +0200

    implement simple frontend ui with header

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page-container">
+      <header className="header">
+        <h1>Retail Product Catalog</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
